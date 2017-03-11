054e56df8d05805a05738a7299b75d38b0c8fad0
rookieJX <wangjiaxiangyx@163.com>
2017-03-11 09:06:40 +0800

    继承父类
    创建发现,我的公共父类

---
+++ JXMicroBlog/Classes/Main/Controller/JXGlobalViewController.h
@@ -0,0 +1,16 @@
+//
+//  JXGlobalViewController.h
+//  JXMicroBlog
+//
+//  Created by 王加祥 on 2017/3/11.
+//  Copyright © 2017年 王加祥. All rights reserved.
+//  全局控制器,主要是发现跟我的父类,因为两个东西基本一样
+
+#import <UIKit/UIKit.h>
+
+@interface JXGlobalViewController : UITableViewController
+
+/** 数组 */
+@property (nonatomic,strong) NSMutableArray * groups;
+
+@end
